13078bb10971c5fc2269853897524faceea7dec7
Andrespeerez <andreslavila93@gmail.com>
2023-03-01 13:29:48 +0100

    Added 01-06.py and updated 01.ipynb with 01-06.py

--- 01-Conceptos-basicos/01-06-trabajando-con-zonas-horarias.py
+++ 01-Conceptos-basicos/01-06-trabajando-con-zonas-horarias.py
@@ -4,5 +4,18 @@
 # en Linea de comandos
 # pip install pytz
 
+from datetime import datetime, timedelta
+from pytz import timezone
 import pytz
 
+# Mostrar las zonas horarias disponibles
+# print(pytz.all_timezones)
+print()
+
+# Mostrar la fecha actual
+dt = datetime.now()
+print(dt)
+print(datetime.now(pytz.timezone("Asia/Tokyo")))
+print(datetime.now(pytz.timezone("Europe/Madrid")))
+print(datetime.now(pytz.timezone("US/Alaska")))
+print(datetime.now(pytz.timezone("UTC")))
